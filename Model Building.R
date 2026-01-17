0c99f65de152e66a28f56981e49dca62da7f9c36
Silvanius <110171336+Mulu007@users.noreply.github.com>
2026-01-16 18:51:02 -0600

    Add files via upload

---
+++ Model Building.R
@@ -0,0 +1,40 @@
+library(tidyverse)
+library(modelr)
+options(na.action = na.warn)
+
+library(nycflights13)
+library(lubridate)
+
+ggplot(diamonds, aes(cut, price)) + geom_boxplot()
+ggplot(diamonds, aes(color, price)) + geom_boxplot()
+ggplot(diamonds, aes(clarity, price)) + geom_boxplot()
+
+ggplot(diamonds, aes(carat, price)) +
+  geom_hex(bins = 50)
+
+diamonds2 <- diamonds %>%
+  filter(carat <= 2.5) %>%
+  # Why use log?
+  # Because price grows faster and faster as carat increases
+  # ie. price = carat^k (k > 1). This is a multiplicative/ power-law relationship
+  # log(price) = log(a) + k.log(carat) > now its linear
+  # Why log base 2?
+  # Just affects interpretation. 1 unit increase = a doubling
+  mutate(lprice = log2(price), lcarat = log2(carat))
+
+ggplot(diamonds2, aes(lcarat, lprice)) +
+  geom_hex(bins = 50)
+
+mod_diamond <- lm(lprice ~ lcarat, data = diamonds2)
+
+grid <- diamonds2 %>%
+  # seq_range(carat, 20) -> takes the range of carat values and creats 20 evenly spaced values
+  data_grid(carat = seq_range(carat, 20)) %>%
+  mutate(lcarat = log2(carat)) %>%
+  # adds predictions from the model storing in lprice
+  add_predictions(mod_diamond, "lprice") %>%
+  mutate(price = 2 ^ lprice)
+
+ggplot(diamonds2, aes(carat, price)) +
+  geom_hex(bins = 50) +
+  geom_line(data = grid, color = "red", size = 1)
